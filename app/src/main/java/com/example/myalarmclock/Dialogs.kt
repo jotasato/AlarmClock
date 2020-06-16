56b2b9fabbd3d93fa4c2778cd227cc085dda2e0b
jotasato <arukanaika.aruka.jj@gmail.com>
2020-06-17 05:55:48 +0900

    日付と時刻を選択するダイアログ実装　スリープを解除して画面を表示する機能実装 アラームアプリ完成

--- app/src/main/java/com/example/myalarmclock/Dialogs.kt
+++ app/src/main/java/com/example/myalarmclock/Dialogs.kt
@@ -1,16 +1,22 @@
 package com.example.myalarmclock
 
+import android.app.DatePickerDialog
 import android.app.Dialog
+import android.app.TimePickerDialog
 import android.content.Context
 import android.os.Bundle
+import android.widget.DatePicker
+import android.widget.TimePicker
 import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.DialogFragment
+import java.util.*
 
 class TimeAlertDialog : DialogFragment() {
     interface Listener {
         fun getUp()
         fun snooze()
     }
+
     //変数listenerにListenerインターフェイスを実装したクラスだけを格納できるようになる。
     private var listener: Listener? = null
 
@@ -24,6 +30,7 @@ class TimeAlertDialog : DialogFragment() {
             is Listener -> listener = context
         }
     }
+
     //DialogFragmentクラスのonCreateDialogメソッドをオーバーライドして、内部でダイアログを作成し、戻り値として返す。
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
         //表示するダイアログの内容は、まずAlertDialog.Builderのインスタンスを作成し、それを使って設定していく。
@@ -51,4 +58,84 @@ class TimeAlertDialog : DialogFragment() {
         // ダイアログのオブジェクトを返さなければならない
         return builder.create()
     }
+}
+
+//DatePickerDialogを使う場合、DialogFragmentクラスを継承する他に、DatePickerDialog.OnDateSetListenerインターフェイスを実装します。
+class DatePickerFragment : DialogFragment(),
+    DatePickerDialog.OnDateSetListener {
+    //インターフェイスを用意する処理は、TimeAlertDialogと同じです。このインターフェイスはアクティビティに実装して、
+    //オーバーライドしたonSelectedメソッドに日付が選択された時の処理を記述する。
+    interface OnDateSelectedListener {
+        //onSelectedメソッドは年、月、日を引数にとる。
+        fun onSelected(year: Int, month: Int, date: Int)
+    }
+
+    private var listener: OnDateSelectedListener? = null
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        when (context) {
+            is OnDateSelectedListener -> listener = context
+        }
+    }
+
+    //onCreateDialogではDatePickerDialogのインスタンスを返すが、その前に現在の日付を初期値として設定している。
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        //カレンダークラスのgetメソッドを使って、年、月、日の値を変数に取得している。
+        val c = Calendar.getInstance()
+        //.getメソッドでCalendarオブジェクトの各フィールドの値を返す。
+        val year = c.get(Calendar.YEAR)
+        val month = c.get(Calendar.MONTH)
+        val date = c.get(Calendar.DAY_OF_MONTH)
+        //DatePickerDialogクラスのコンストラクタ
+        //第1引数はダイアログを表示する対象のコンテキスト。第2引数は、日付がセットされた時に呼ばれるリスナー。
+        //第3引数は初期設定をする年、第4引数は初期設定をする月、第5引数は初期設定をする日
+        return DatePickerDialog(requireActivity(), this, year, month, date)
+    }
+
+    //日付が選択された時に呼ばれる。
+    override fun onDateSet(
+        view: DatePicker?, year: Int,
+        month: Int, dayOfMonth: Int
+    ) {
+        listener?.onSelected(year, month, dayOfMonth)
+    }
+}
+
+//基本的な流れは、DatePickerDialogの場合と同じ
+//TimePickerDialogを使う場合、DialogFragmentクラスを継承する他に、TimePickerDialog.OnTimeSetListenerインターフェイスを実装する
+
+class TimePickerFragment : DialogFragment(),
+    TimePickerDialog.OnTimeSetListener {
+    interface OnTimeSelectedListener {
+        fun onSelected(hourOfDay: Int, minute: Int)
+    }
+
+    private var listener: OnTimeSelectedListener? = null
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        when (context) {
+            is OnTimeSelectedListener -> listener = context
+        }
+    }
+
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        val c = Calendar.getInstance()
+        val hour = c.get(Calendar.HOUR_OF_DAY)
+        val minute = c.get(Calendar.MINUTE)
+        //TimePickerDialogのインスタンスを返している。
+        //コンストラクタの第1引数は、ダイアログを表示する対象のアクティビティ、第2引数は、時刻がセットされた時に呼ばれるリスナー、
+        //第3引数は初期設定する時、第4引数は初期設定する分、第5引数はtrueなら24時間表記、falseならAM/PM表記
+        return TimePickerDialog(context, this, hour, minute, true)
+    }
+
+    //onTimeSetメソッドは、時刻が選択された時に呼ばれる。
+    //第1引数viewはダイアログに関連老けられたTimePickerビュー、第2引数は時、第3引数は分
+    //内部ではonSelectedメソッドを実行しているだけ。このメソッドの中身もアクティビティで実装する。
+    override fun onTimeSet(view: TimePicker?, hourOfDay: Int, minute: Int) {
+        listener?.onSelected(hourOfDay, minute)
+    }
+
+
 }
--- app/src/main/java/com/example/myalarmclock/MainActivity.kt
+++ app/src/main/java/com/example/myalarmclock/MainActivity.kt
@@ -1,20 +1,42 @@
 package com.example.myalarmclock
 
 import android.app.AlarmManager
+import android.app.KeyguardManager
 import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
 import android.os.Build
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.Parcel
-import android.os.Parcelable
+import android.text.format.DateFormat
+import android.view.WindowManager.LayoutParams.*
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
+import java.text.ParseException
+import java.text.SimpleDateFormat
 import java.util.*
 
-class MainActivity() : AppCompatActivity(), TimeAlertDialog.Listener, Parcelable {
-    constructor(parcel: Parcel) : this() {
+class MainActivity : AppCompatActivity(), TimeAlertDialog.Listener
+    //DatePickerFragmentとTimePickerFragmentで定義したインターフェイスを実装
+    , DatePickerFragment.OnDateSelectedListener
+    , TimePickerFragment.OnTimeSelectedListener {
+
+    //日付選択ダイアログの選択が終了したら呼ばれるところ。
+    override fun onSelected(year: Int, month: Int, date: Int) {
+        //カレンダーオブジェクトを作成。
+        val c = Calendar.getInstance()
+        //ダイアログで選択された年、月、日を設定
+        c.set(year, month, date)
+        //文字列に変換してテキストビューに表示しています。
+        //DateFormatクラスメソッドであるformatメソッドを使って、Calendarオブジェクトを
+        //フォーマット済み文字列に変換している。
+        dateText.text = DateFormat.format("yyyy/MM/dd", c)
+    }
+
+    //時刻選択ダイアログの選択が終了したら呼ばれるところ。
+    //メソッドの中では選択された時と分をtimeTextに設定する
+    override fun onSelected(hourOfDay: Int, minute: Int) {
+        timeText.text = "%1$02d:%2$02d".format(hourOfDay, minute)
     }
 
     //getUpメソッドは、ダイアログで「起きる」ボタンが押された時に呼ばれる。
@@ -32,35 +54,87 @@ class MainActivity() : AppCompatActivity(), TimeAlertDialog.Listener, Parcelable
         finish()
 
     }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         //インテントのエクストラとして"onReceive"にtrueが指定されているかチェックしています。
         //アクティビティの起動に使用されたインテントの取得はgetIntentメソッドで行いますが、Kotlinではintentプロパティが利用できます。
         if (intent?.getBooleanExtra("onReceive", false) == true) {
+            when {
+                Build.VERSION.SDK_INT >= Build.VERSION_CODES.O_MR1 -> {
+                    //アクティビティが終了した時に画面を表示するかどうか。trueの場合Acticityをロック画面上に表示
+                    setShowWhenLocked(true)
+                    //アクティビティが再開した時に画面をONにするかどうか。trueの場合画面をONにする
+                    setTurnScreenOn(true)
+                    //キーボードの解除にKeyguardManagerクラスのインスタンスを取得する必要がある
+                    val keyguardManager =
+                        getSystemService(Context.KEYGUARD_SERVICE) as KeyguardManager
+                    //デバイスのキーガード（ロック画面を解除している）
+                    keyguardManager.requestDismissKeyguard(this, null)
+                }
+                Build.VERSION.SDK_INT >= Build.VERSION_CODES.O -> {
+                    window.addFlags(
+                        FLAG_TURN_SCREEN_ON or FLAG_SHOW_WHEN_LOCKED
+                    )
+                    val keyguardManager =
+                        getSystemService(Context.KEYGUARD_SERVICE) as KeyguardManager
+                    keyguardManager.requestDismissKeyguard(this, null)
+                }
+                else ->
+                    //addFlagsメソッドデフラグに従って、Windowのフラグを設定する
+                    window.addFlags(
+                        FLAG_TURN_SCREEN_ON or FLAG_SHOW_WHEN_LOCKED or FLAG_DISMISS_KEYGUARD
+                    )
+            }
             //TimeAlertDialogのインスタンスを生成し、showメソッドで表示している。
             val dialog = TimeAlertDialog()
             dialog.show(supportFragmentManager, "alert_dialog")
         }
-
         setContentView(R.layout.activity_main)
 
 
-        //setAlarmボタンをタップした時にCalendarクラスで現在の時間より5秒後の時間を作成している。
+        //アラームをセットするボタンが押されたら、ダイアログで選択した日付、時刻でアラームを設定する。
         setAlarm.setOnClickListener {
-            //Calendarクラスのインスタンスは、getInstanceメソッドで取得する必要があります。
-            val calendar = Calendar.getInstance()
-            //System.currentTimeMillis()で現在の時刻を取得。//.timeInMillisで取得した現在の時刻をミリ秒で設定
-            calendar.timeInMillis = System.currentTimeMillis()
-            //addメソッドはCalendarに設定している時刻を編集する。引数には編集したい場所と値を渡します。
-            //今回の場合だと単位に秒を表すCalendar.SECONDを編集したいので指定して、値に5を渡すことで時刻を5秒進めている。
-            //変数calendarには現在の時刻を秒数化したものが入っているので、現在時刻の5病後という設定になる。
-            calendar.add(Calendar.SECOND, 5)
-            //Calendarクラスのインスタンスである変数calendarをsetAlarmManagerに渡している。
-            setAlarmManager(calendar)
+            //各テキストビューdateText(日付)、timeText(時刻)を取り出しDate型に変換して変数dateに代入している。
+            //toDate()という関数は下の方で定義してあるString型の拡張関数
+            val date = "${dateText.text} ${timeText.text}".toDate()
+            when {
+                //変数dateがnullでない場合は、アラームをセットされましたのメッセージを表示し、
+                //nullの場合はエラーメッセージを表示するようにしている。
+                date != null -> {
+                    //Calendarクラスのインスタンスは、getInstanceメソッドで取得する必要があります。
+                    val calendar = Calendar.getInstance()
+                    calendar.time = date
+                    //Calendarクラスのインスタンスである変数calendarをsetAlarmManagerに渡している。
+                    setAlarmManager(calendar)
+                    Toast.makeText(
+                        this, "アラームをセットしました",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+                else -> {
+                    Toast.makeText(
+                        this, "日付の形式が正しくありません",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+            }
         }
 
-        cancelAlarm.setOnClickListener{
+        cancelAlarm.setOnClickListener {
             cancelAlarmManager()
+        }
+
+        //dateTextをタップした時、日付選択ダイアログを表示。選択が終了したらonSelectedメソッドが呼ばれる
+        dateText.setOnClickListener {
+            val dialog = DatePickerFragment()
+            dialog.show(supportFragmentManager, "data_dialog")
+        }
+
+        //timeTextをタップした時、時刻選択ダイアログを表示。選択が終了したらonSelectedメソッドが呼ばれる
+        timeText.setOnClickListener {
+            val dialog = TimePickerFragment()
+            dialog.show(supportFragmentManager, "time_dialog")
         }
 
     }
@@ -79,7 +153,8 @@ class MainActivity() : AppCompatActivity(), TimeAlertDialog.Listener, Parcelable
             //設定するアラームの時刻とアラーム設定のためのインテントを指定する。今回はアラーム設定のためのインテントを使用しないので、第2引数にはnullを渡す。
             Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP -> {
                 val info = AlarmManager.AlarmClockInfo(
-                    calendar.timeInMillis, null)
+                    calendar.timeInMillis, null
+                )
                 //AlarmManager.AlarmClockInfoのインスタンスである変数infoをsetAlarmClockメソッドに渡す。
                 //setAlarmClockメソッドでアラームを設定している。第1引数がアラーム情報で、第2引数がアラーム時刻になった時に実行するインテント。
                 am.setAlarmClock(info, pending)
@@ -90,12 +165,14 @@ class MainActivity() : AppCompatActivity(), TimeAlertDialog.Listener, Parcelable
                 //アラームのタイプには、RTC_WAKEUPを渡し、デバイスオフの場合にはデバイスを起動するようにしている。
                 //setExactメソッドはアラームが正確に配信されるようにスケジュールします。第1引数がアラームのタイプ,
                 // 第2引数が設定するアラームの時刻,第3引数がアラーム実行になった時に実行するインテント。
-                am.setExact(AlarmManager.RTC_WAKEUP,calendar.timeInMillis, pending)
+                am.setExact(AlarmManager.RTC_WAKEUP, calendar.timeInMillis, pending)
             }
             else -> {
                 ////それ以前の場合の処理。setメソッドを使っている。使い方はsetExactと同じ
-                am.set(AlarmManager.RTC_WAKEUP,
-                calendar.timeInMillis, pending)
+                am.set(
+                    AlarmManager.RTC_WAKEUP,
+                    calendar.timeInMillis, pending
+                )
             }
         }
     }
@@ -109,21 +186,18 @@ class MainActivity() : AppCompatActivity(), TimeAlertDialog.Listener, Parcelable
         am.cancel(pending)
     }
 
-    override fun writeToParcel(parcel: Parcel, flags: Int) {
-
-    }
-
-    override fun describeContents(): Int {
-        return 0
-    }
-
-    companion object CREATOR : Parcelable.Creator<MainActivity> {
-        override fun createFromParcel(parcel: Parcel): MainActivity {
-            return MainActivity(parcel)
-        }
-
-        override fun newArray(size: Int): Array<MainActivity?> {
-            return arrayOfNulls(size)
+    private fun String.toDate(pattern: String = "yyyy/MM/dd HH:mm"): Date? {
+        return try {
+            //Dateに変換したい文字列の形式を指定してSimpleDateFormatクラスのインスタンスを生成し、
+            //parseメソッドに文字列フォーマットを渡して、Date型に変換している。
+            SimpleDateFormat(pattern).parse(this)
+            //文字列が意図した形式ではない場合などにはIllegalArgumentException例外、ParseException例外が発生しますので、
+            //try-catch処理を入れる必要があります。
+            //拡張関数は自分自身を処理するので、その内部では拡張するオブジェクトをthisとして参照できることに注意する。
+        } catch (e: IllegalArgumentException) {
+            return null
+        } catch (e: ParseException) {
+            return null
         }
     }
 
